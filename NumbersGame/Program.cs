6365a4740db9b6c4863e13d25328c8c888801654
willi <willi@WILLIAMS_LAPTOP>
2026-09-16 11:42:20 +0200

    Add project files.

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace NumbersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök!");
+
+            Console.WriteLine(CheckGuess(0, 0));
+
+          }
+
+        static string CheckGuess(int guess, int secret)
+        {
+            Random random = new Random();
+            secret = random.Next(1, 20);
+
+            Console.WriteLine("Gissa ett tal:");
+
+            int atempts = 0;
+
+            while (guess != secret && atempts != 5)
+            {
+                string userInput = Console.ReadLine();
+                guess = int.Parse(userInput);
+                if (guess > secret)
+                {
+                    Console.WriteLine("Tyvärr, du gissade för högt!");
+                    atempts++;
+                }
+                else if (guess < secret)
+                {
+                    Console.WriteLine("Tyvärr, du gissade för lågt!");
+                    atempts++;
+                }
+            }
+            if (atempts == 5)
+            {
+                return "Tyvärr, du lyckades inte gissa talet på fem försök!";
+            }
+            else
+            return "Wohoo! Du klarade det!";
+
+        }
+    }
+}
